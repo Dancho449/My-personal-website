6b2d3afa980f1465117128674e379cc283db289c
Dancho449 <125226463+Dancho449@users.noreply.github.com>
2023-06-12 13:09:36 -0700

    new change

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const menu = document.querySelector('.sidebar');
 const clear = document.querySelector('.sidebar-clear');
 const body = document.querySelector('.body');
 const container = document.querySelector('.side-menu');
+const icon2 = document.querySelector('.light-dark2');
+const ham = document.querySelector('.ham');
 
 btn.addEventListener('click', function(){
     menu.style.width = "clamp(180px, 27%, 360px)"
@@ -18,5 +20,16 @@ window.addEventListener('click', function(e){
     if(e.target === container){
         menu.style.width = "0"
         container.style.width = "0"
+    }
+})
+
+icon2.addEventListener('click', function() {
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon2.src = "./images/sun.PNG"
+        ham.src = "./images/menubar-light.png"
+    }else{
+        icon2.src = "./images/moon.PNG"
+        ham.src = "./images/menubar-light.png"
     }
 })
